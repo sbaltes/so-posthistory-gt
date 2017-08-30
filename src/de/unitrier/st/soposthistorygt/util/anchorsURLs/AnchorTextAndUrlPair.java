10f765e36261d4c729a930d32911df09b6e64cbe
Lorik Dumani <s4loduma@uni-trier.de>
2017-08-30 18:09:16 +0200

    fixed bug so that multiple anchor texts with the same reference will be normalized instead of the first
    fixed #17
    refactored code: separated regex for markdown link reference to make it easier to assign one url to multiple anchor texts .

--- src/de/unitrier/st/soposthistorygt/util/anchorsURLs/AnchorTextAndUrlPair.java
+++ src/de/unitrier/st/soposthistorygt/util/anchorsURLs/AnchorTextAndUrlPair.java
@@ -13,12 +13,13 @@ public class AnchorTextAndUrlPair {
 
 
     public enum AnchorRefUrlType{               // https://stackoverflow.com/editing-help#code
-        // spanClassWithAnchorTextAndDirectURL,    // Here's a <span class="hi">[poorly-named link](http://www.google.com/ "Google")</span>. // TODO for Sebastian: ignore this case because it never appeared yet?
+        // spanClassWithAnchorTextAndDirectURL, // Here's a <span class="hi">[poorly-named link](http://www.google.com/ "Google")</span>. // TODO for Sebastian: ignore this case because it never appeared yet?
         type_markdownLinkBareTags,              // Have you ever seen <http://example.com>?
         type_anchorLink,                        // <a href="http://example.com" title="example">example</a>
         type_markdownLinkInline,                // e.g. Here's an inline link to [Google](http://www.google.com/).
-        type_markdownLinkReference,             // e.g. Here's a reference-style link to [Google][1].   ...     and later   ...     [1]: http://www.google.com/
-        type_bareURL,                                // I often visit http://example.com.
+        type_markdownLinkReference_top,         // e.g. Here's a reference-style link to [Google][1].   ...     and later   ...     [1]: http://www.google.com/
+        type_markdownLinkReference_bottom,      // e.g. Here's a reference-style link to [Google][1].   ...     and later   ...     [1]: http://www.google.com/
+        type_bareURL,                           // I often visit http://example.com.
     }
 
     public static Pattern getRegexWithEnum(AnchorRefUrlType type){
@@ -31,8 +32,10 @@ public class AnchorTextAndUrlPair {
                 return AnchorLink.regex;
             case type_markdownLinkInline:
                 return MarkdownLinkInline.regex;
-            case type_markdownLinkReference:
-                return MarkdownLinkReference.regex;
+            case type_markdownLinkReference_top:
+                return MarkdownLinkReference.regex_top;
+            case type_markdownLinkReference_bottom:
+                return MarkdownLinkReference.regex_bottom;
             case type_bareURL:
                 return Link.regex;
 
