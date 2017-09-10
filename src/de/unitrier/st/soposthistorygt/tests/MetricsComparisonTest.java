957b66ad8d206963a9fcea646a5e4e2549485332
Lorik Dumani <s4loduma@uni-trier.de>
2017-09-10 18:14:31 +0200

    added testcases to check 1. whether the number of predecessors is at most one (failes) 2. whether the post version links will be reset right

--- src/de/unitrier/st/soposthistorygt/tests/MetricsComparisonTest.java
+++ src/de/unitrier/st/soposthistorygt/tests/MetricsComparisonTest.java
@@ -1,18 +1,18 @@
 package de.unitrier.st.soposthistorygt.tests;
 
 import de.unitrier.st.soposthistory.blocks.TextBlockVersion;
+import de.unitrier.st.soposthistory.version.PostVersionList;
 import de.unitrier.st.soposthistorygt.metricsComparism.ConnectionsOfAllVersions;
 import de.unitrier.st.soposthistorygt.metricsComparism.GroundTruthExtractionOfCSVs;
 import de.unitrier.st.soposthistorygt.metricsComparism.MetricsComparator;
 import de.unitrier.st.soposthistorygt.metricsComparism.PostVersionsListManagement;
-import de.unitrier.st.stringsimilarity.fingerprint.Variants;
-import de.unitrier.st.stringsimilarity.profile.Base;
 import org.apache.commons.lang3.time.StopWatch;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class MetricsComparisonTest {
 
@@ -25,6 +25,7 @@ public class MetricsComparisonTest {
 
         GroundTruthExtractionOfCSVs groundTruthExtractionOfCSVs = new GroundTruthExtractionOfCSVs(pathToCSVs.toString());
         PostVersionsListManagement postVersionsListManagement = new PostVersionsListManagement(pathToCSVs.toString());
+        postVersionsListManagement.getPostVersionListWithID(postId).processVersionHistory();
 
         ConnectionsOfAllVersions connectionsOfAllVersionsGroundTruth_text = groundTruthExtractionOfCSVs.getAllConnectionsOfAllConsecutiveVersions_text(postId);
         ConnectionsOfAllVersions connectionsOfAllVersionsGroundTruth_code = groundTruthExtractionOfCSVs.getAllConnectionsOfAllConsecutiveVersions_code(postId);
@@ -73,11 +74,19 @@ public class MetricsComparisonTest {
     @Test
     public void testNumberOfPredecessors(){
         int postId = 3758880;
+        TextBlockVersion.similarityMetric = de.unitrier.st.stringsimilarity.set.Variants::twoGramDiceVariant;
 
         GroundTruthExtractionOfCSVs groundTruthExtractionOfCSVs = new GroundTruthExtractionOfCSVs(pathToCSVs.toString());
-
-        TextBlockVersion.similarityMetric = de.unitrier.st.stringsimilarity.profile.Variants::tokensProfileCosineNormalized;
         PostVersionsListManagement postVersionsListManagement = new PostVersionsListManagement(pathToCSVs.toString());
+        postVersionsListManagement.getPostVersionListWithID(postId).processVersionHistory(PostVersionList.PostBlockTypeFilter.TEXT);
+
+
+        List<TextBlockVersion> textBlocks = postVersionsListManagement.getPostVersionListWithID(postId).get(postVersionsListManagement.getPostVersionListWithID(postId).size()-1).getTextBlocks();
+        for(int i=0; i<textBlocks.size(); i++){
+            System.out.println(textBlocks.get(i).getLocalId() + " has pred " + textBlocks.get(i).getPred().getLocalId());
+        }
+        System.out.println();
+
 
         ConnectionsOfAllVersions connectionsOfAllVersionsGroundTruth_text = groundTruthExtractionOfCSVs.getAllConnectionsOfAllConsecutiveVersions_text(postId);
         ConnectionsOfAllVersions connectionsOfAllVersionsComputedMetric_text = postVersionsListManagement.getAllConnectionsOfAllConsecutiveVersions_text(postId);
@@ -90,6 +99,34 @@ public class MetricsComparisonTest {
         }
 
         System.out.println("\n\nComputed Metric: ");
+        System.out.println("All text blocks:");
+        for(int i=0; i<connectionsOfAllVersionsComputedMetric_text.size(); i++){
+            System.out.println(connectionsOfAllVersionsComputedMetric_text.get(i));
+        }
+    }
+
+    @Test
+    public void checkWhetherPostVersionListConnectionsWillBeResetRight(){
+        int postId = 3758880;
+        TextBlockVersion.similarityMetric = de.unitrier.st.stringsimilarity.fingerprint.Variants::winnowingTokenDiceVariant;
+
+        PostVersionsListManagement postVersionsListManagement = new PostVersionsListManagement(pathToCSVs.toString());
+        postVersionsListManagement.getPostVersionListWithID(postId).processVersionHistory(PostVersionList.PostBlockTypeFilter.TEXT);
+
+
+        ConnectionsOfAllVersions connectionsOfAllVersionsComputedMetric_text = postVersionsListManagement.getAllConnectionsOfAllConsecutiveVersions_text(postId);
+
+        System.out.println("\n\nComputed Metric after processVersionHistory: ");
+        System.out.println("All text blocks:");
+        for(int i=0; i<connectionsOfAllVersionsComputedMetric_text.size(); i++){
+            System.out.println(connectionsOfAllVersionsComputedMetric_text.get(i));
+        }
+
+
+        postVersionsListManagement = new PostVersionsListManagement(pathToCSVs.toString());
+        connectionsOfAllVersionsComputedMetric_text = postVersionsListManagement.getAllConnectionsOfAllConsecutiveVersions_text(postId);
+
+        System.out.println("\n\nComputed Metric after resetting of links: ");
         System.out.println("All text blocks:");
         for(int i=0; i<connectionsOfAllVersionsComputedMetric_text.size(); i++){
             System.out.println(connectionsOfAllVersionsComputedMetric_text.get(i));
